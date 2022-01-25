c58406e8407cfc4ebb8afc2495434bd43fc11a6f
Jackmatthews2 <98429741+Jackmatthews2@users.noreply.github.com>
2022-01-25 21:46:58 +0000

    Add files via upload

---
+++ ProcessCSV.ps1
@@ -0,0 +1,14 @@
+﻿$csv = Import-Csv "C:\Users\JackNEW\Documents\_Jack\_spreadsheets\Budgetting\oct18\oct18.csv"
+
+# Not working yet
+$csv | select *,@{name="type";expression={
+        $payment = $_.payment; ? {$_.payment -like "*apple*"}; $payment
+    }
+}
+
+<# Notes
+$wmiServices = Get-CimInstance -ClassName win32_service -Property Name,PathName
+
+Get-Service | Select-Object -Property *,
+@{Name = 'PathName'; Expression = { $serviceName = $_.Name; (@($wmiServices).where({ $_.Name -eq $serviceName })).PathName }}
+#>
